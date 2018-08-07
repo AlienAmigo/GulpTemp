626fd65d628d9d00397062a2eb45d25369d3e168
AlienAmigo <maestro_pete@inbox.ru>
2018-08-07 13:18:12 +0300

    Ready Minimum without pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 
 // ОСНОВНЫЕ ПЕРЕМЕННЫЕ И НАСТРОЙКИ
 const gulp = require('gulp'),
-			sass = require('gulp-sass');
+			sass = require('gulp-sass'),
+			browserSync = require('browser-sync');
 
 var dirs = {
-    "srcPath": "./src/",
-    "buildPath": "./build/",
+    "srcPath": "./src/", // в оригинале ./app/
+    "buildPath": "./build/", // в оригинале ./dist/
     "blocksDirName": "blocks"
   }
 
@@ -23,14 +24,30 @@ var dirs = {
 // });
 
 
-
-// попытка сделать 
+gulp.task('browser-sync', function() {
+	browserSync({
+	server: {
+		baseDir: dirs.srcPath
+	},
+	notify: true
+	});
+})
+// компиляция SСSS в CSS
 gulp.task('sass', function(){
 	return gulp.src(dirs.srcPath+'scss/*.scss')
 	.pipe(sass())
-	.pipe(gulp.dest(dirs.srcPath+'/css'));
+	.pipe(gulp.dest(dirs.srcPath+'/css'))
+	.pipe(browserSync.reload({stream: true}));
 })
 
+
+
+gulp.task('watch', gulp.parallel('browser-sync', 'sass'), function() {
+	gulp.watch(dirs.srcPath+'scss/*.scss', gulp.parallel(['sass']));
+	gulp.watch(dirs.srcPath+'**/*.html', browserSync.reload);
+	gulp.watch(dirs.srcPath+'js/*.js', browserSync.reload);
+
+}) 
 // Тестовое задание
 // gulp.task("mytask", function(done){
 // 	return gulp.src('source-files')
