b3860d528f16a8e9effd43b53833fc991127cf42
Pete <maestro_pete@inbox.ru>
2019-01-16 13:24:08 +0300

    SASS added

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict'
+// 'use strict'
 
 // ОСНОВНЫЕ ПЕРЕМЕННЫЕ И НАСТРОЙКИ
 const gulp = require('gulp'),
@@ -9,7 +9,7 @@ var dirs = {
     "srcPath": "./src/", // в оригинале ./app/
     "buildPath": "./build/", // в оригинале ./dist/
     "blocksDirName": "blocks"
-  }
+  };
 
 
 //ЗАДАЧИ GULP 
@@ -31,23 +31,24 @@ gulp.task('browser-sync', function() {
 	},
 	notify: true
 	});
-})
+});
+
 // компиляция SСSS в CSS
 gulp.task('sass', function(){
 	return gulp.src(dirs.srcPath+'scss/*.scss')
 	.pipe(sass())
-	.pipe(gulp.dest(dirs.srcPath+'/css'))
+	.pipe(gulp.dest(dirs.buildPath+'/css'))
 	.pipe(browserSync.reload({stream: true}));
-})
+});
 
 
 
 gulp.task('watch', gulp.parallel('browser-sync', 'sass'), function() {
-	gulp.watch(dirs.srcPath+'scss/*.scss', gulp.parallel(['sass']));
-	gulp.watch(dirs.srcPath+'**/*.html', browserSync.reload);
-	gulp.watch(dirs.srcPath+'js/*.js', browserSync.reload);
+	gulp.watch(dirs.srcPath+'scss/*.scss', gulp.parallel(['sass']))
+  gulp.watch(dirs.srcPath+'**/*.html', browserSync.reload)
+	gulp.watch(dirs.srcPath+'js/*.js', browserSync.reload)
 
-}) 
+}); 
 // Тестовое задание
 // gulp.task("mytask", function(done){
 // 	return gulp.src('source-files')
